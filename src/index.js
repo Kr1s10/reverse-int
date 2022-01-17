bbb11405fd0798b32074454d936e74ec4be39d12
Kristen <kr1s10.khud@gmail.com>
2022-01-18 01:46:31 +0200

    feat: add reverse func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let result = `${n}`;
+    if (n < 0) {
+        return +result.slice(1).split('').reverse().join('');
+    }
+    return +result.split('').reverse().join('');
 }
